9d4948ac108309be067820b17d6707fe7445d753
ASIS PAL <asispals16@example.com>
2026-06-21 14:31:45 +0530

    Changes to be added

--- src/asisPal/Example1.java
+++ src/asisPal/Example1.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import technocredits.base.BrowserActions;
+import technocredits.technoapp.base.BrowserActions;
 import org.testng.annotations.Test;
 
 // Selenium ni jar add na karvi padi ? kayu ?
