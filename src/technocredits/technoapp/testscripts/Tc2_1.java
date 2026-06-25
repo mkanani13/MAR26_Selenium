23d7f8c8a6647c1b843b2564b9122c2b1e3689ac
unknown <kanani.maulik@gmail.com>
2026-06-25 09:29:00 +0530

    session code

--- src/technocredits/technoapp/testscripts/Tc2_1.java
+++ src/technocredits/technoapp/testscripts/Tc2_1.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import technocredits.technoapp.base.BrowserActions;
 import technocredits.technoapp.pages.LoginPage;
 import technocredits.technoapp.pages.RestaurantsPage;
@@ -37,11 +38,17 @@ public class Tc2_1 {
         restaurantsPage.setLocationInDropdown("Baner");
 
         System.out.println("STEP - Click on View & Order of the restaurants having dishes");
+        String restaurantName = driver.findElement(By.xpath("(//div[@data-testid = 'restaurants-grid']//p[not(contains(text(),' 0 dishes'))])[1]/preceding-sibling::h3")).getText().trim();
         driver.findElement(By.xpath("(//div[@data-testid = 'restaurants-grid']//p[not(contains(text(),' 0 dishes'))]/following::a[1])[1]")).click();
 
         System.out.println("STEP - From the restaurant menu, locate the first food item whose Stock value is greater than 0 and increase its quantity by clicking the Qty Up Arrow.");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@data-testid='proceed-checkout-btn']")));
+
+        System.out.println("VERIFY - Navigated to Selected Restaurant Menu");
+        String restaurantMenu_RestaurantName = driver.findElement(By.xpath("//h2[@data-testid='restaurant-name']")).getText();
+        Assert.assertEquals(restaurantMenu_RestaurantName, restaurantName);
+
         String dish = driver.findElement(By.xpath("(//table[@data-testid='menu-table']/tbody/tr/td[4][text()!='0'])[1]/preceding-sibling::td[3]/div/div/div[1]")).getText();
         WebElement qElement  = driver.findElement(By.xpath("(//table[@data-testid='menu-table']/tbody/tr/td[4][text()!='0'])[1]/following-sibling::td[1]/input"));
         qElement.click();
@@ -76,6 +83,58 @@ public class Tc2_1 {
         System.out.println("VERIFY - that the amount displayed in the Payable section of the Order Summary screen matches the amount displayed in the Total Payable section of the Bill screen.");
         Assert.assertEquals(paymentSummaryTotalPayableAmt, orderSummaryTotalPayableAmt);
 
+        System.out.println("STEP - Click on Pay & Place Order");
+        driver.findElement(By.xpath("//button[@id='pay-btn']")).click();
 
+        System.out.println("VERIFY - Confirm the capta error notification displayed");
+        boolean captaFlag = driver.findElement(By.xpath("//div[text()='Please confirm the captcha before paying']")).isDisplayed();
+        Assert.assertTrue(captaFlag);
+
+        System.out.println("STEP - Click on Capta checkbox");
+        driver.findElement(By.xpath("//input[@id='pay-captcha']")).click();
+
+        System.out.println("STEP - Click on Pay & Place Order");
+        driver.findElement(By.xpath("//button[@id='pay-btn']")).click();
+
+        System.out.println("VERIFY - Confirm the UPI ID error notification displayed");
+        boolean upiFlag = driver.findElement(By.xpath("//div[text()='Enter a valid UPI ID (name@bank)']")).isDisplayed();
+        Assert.assertTrue(upiFlag);
+
+        System.out.println("STEP - Enter a valid UPI ID in the payment field.");
+        driver.findElement(By.xpath("//input[@id='f-upi']")).sendKeys("mkanani@okhdfcbank");
+
+        System.out.println("STEP - Click on Pay & Place Order");
+        driver.findElement(By.xpath("//button[@id='pay-btn']")).click();
+
+        SoftAssert softAssert = new SoftAssert();
+        System.out.println("VERFIY - that the Order Placed screen is displayed and contains the following details under the Track Order section");
+        boolean orderFlag = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[text()='Order placed!']"))).isDisplayed();
+
+        System.out.println("VERIFY - Order Placed text is displayed");
+        softAssert.assertTrue(orderFlag);
+
+        System.out.println("VERIFY - Restaurant Details");
+        String orderSummaryPage_restuarantName = driver.findElement(By.xpath("//strong")).getText();
+        softAssert.assertEquals(orderSummaryPage_restuarantName, restaurantName);
+
+        System.out.println("VERFIY - Amount Paid");
+        String orderSummaryPage_amountPaid = driver.findElement(By.xpath("//span[@data-testid='success-amount']")).getText();
+        softAssert.assertEquals(orderSummaryPage_amountPaid,paymentSummaryTotalPayableAmt);
+
+        System.out.println("VERIFY - Order id format is as expected");
+        String orderSummaryPage_orderId = driver.findElement(By.xpath("//span[@data-testid='success-order-number']")).getText();
+        softAssert.assertTrue(orderSummaryPage_orderId.startsWith("ORDER#"));
+
+        System.out.println("VERIFY - Payment Method (Paid Via)");
+        String orderSummaryPage_paymentMethod = driver.findElement(By.xpath("//span[text()='Paid via']/following-sibling::span")).getText();
+        softAssert.assertEquals(orderSummaryPage_paymentMethod, "UPI");
+        softAssert.assertAll();
+
+        System.out.println("STEP - Click on View my orders");
+        driver.findElement(By.xpath("//a[@data-testid='success-view-orders']")).click();
+
+        System.out.println("VERIFY - Order is displayed in the list");
+        
     }
+
 }
